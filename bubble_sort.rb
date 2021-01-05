7c934b7a42bd6768457d7f42f54c5ad6d5c81e20
AymenBida <aymen.bida.00@gmail.com>
2021-01-05 16:51:10 +0100

    Correct more linter offenses

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,9 @@
+
 def bubble_sort(arr)
   limit = arr.length
-  while true
+  loop do
     counter = false
-    for i in 0..limit - 2
+    (0..limit - 2).each do |i|
       if arr[i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         counter = true
@@ -16,9 +17,9 @@ end
 
 def bubble_sort_by(arr)
   limit = arr.length
-  while true
+  loop do
     counter = false
-    for i in 0..limit - 2
+    (0..limit - 2).each do |i|
       if yield(arr[i], arr[i + 1]).positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         counter = true
@@ -29,3 +30,6 @@ def bubble_sort_by(arr)
   end
   arr
 end
+
+p bubble_sort([2, 6, 8, 5, 4, 10, 15, 1])
+p bubble_sort_by(["hi", "hello", "hey"]) { |left, right| left.length - right.length }
